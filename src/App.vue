e0bed866ea32c678b5e397ec31e67b548ed64300
hirotoshi <u.hirotoshi@gmail.com>
2019-11-12 21:23:35 +0900

    add MainPage

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainPage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainPage from './components/MainPage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MainPage
   }
 }
 </script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <span>
+    MainPageです
+  </span>
+</template>
+
+<script>
+  export default {
+    name: "MainPage"
+  }
+</script>
+
+<style scoped>
+
+</style>
